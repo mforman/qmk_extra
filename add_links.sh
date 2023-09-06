cccda588235b8e5ffd485d1f8585c40d6bb30561
Michael Forman <michael.forman@oaknorth.com>
2023-09-06 11:46:46 +0100

    Update script file to create links correctly

--- add_links.sh
+++ add_links.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-SCRIPT_PATH=$(dirname $(realpath -s $0))
-QMK_PATH=${1:-$(pwd)}
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+QMK_DIR=$(realpath ${1:-$(pwd)})
 
-echo "Local: $SCRIPT_PATH"
-echo "QMK:   $QMK_PATH"
+echo "Local: $SCRIPT_DIR"
+echo "QMK:   $QMK_DIR"
 
 
-ln -sf $SCRIPT_PATH/users/mforman $QMK_PATH/users/mforman
-ln -sf $SCRIPT_PATH/keyboards/crkbd/keymaps/mforman $QMK_PATH/keyboards/crkbd/keymaps/mforman
-ln -sf $SCRIPT_PATH/keyboards/ergodox_ez/keymaps/mforman $QMK_PATH/keyboards/ergodox_ez/keymaps/mforman
-ln -sf $SCRIPT_PATH/keyboards/gboards/gergoplex/keymaps/mforman $QMK_PATH/keyboards/gboards/gergoplex/keymaps/mforman
+ln -sf $SCRIPT_DIR/users/mforman $QMK_DIR/users/mforman
+ln -sf $SCRIPT_DIR/keyboards/crkbd/keymaps/mforman $QMK_DIR/keyboards/crkbd/keymaps/mforman
+ln -sf $SCRIPT_DIR/keyboards/ergodox_ez/keymaps/mforman $QMK_DIR/keyboards/ergodox_ez/keymaps/mforman
+ln -sf $SCRIPT_DIR/keyboards/gboards/gergoplex/keymaps/mforman $QMK_DIR/keyboards/gboards/gergoplex/keymaps/mforman
 
 find * -type l -not -exec grep -q "^{}$" .gitignore \; -print >> .gitignore
